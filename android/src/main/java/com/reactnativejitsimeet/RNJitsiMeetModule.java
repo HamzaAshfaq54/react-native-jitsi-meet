fa6b112da137cda80c05b762e6fac8936c86157c
HamzaAshfaq <hamza.ashfaq@khaleef.com>
2021-06-14 13:47:43 +0500

    Mute/Unmute and Messages Native Module added

--- android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
+++ android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
@@ -1,24 +1,44 @@
 package com.reactnativejitsimeet;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
-import java.net.URL;
-import java.net.MalformedURLException;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.module.annotations.ReactModule;
-import com.facebook.react.bridge.ReadableMap;
+
+import org.jitsi.meet.sdk.BroadcastEvent;
+import org.jitsi.meet.sdk.BroadcastIntentHelper;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import timber.log.Timber;
+
 
 @ReactModule(name = RNJitsiMeetModule.MODULE_NAME)
 public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
     public static final String MODULE_NAME = "RNJitsiMeetModule";
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onBroadcastReceived(intent);
+        }
+    };
     private IRNJitsiMeetViewReference mJitsiMeetViewReference;
 
     public RNJitsiMeetModule(ReactApplicationContext reactContext, IRNJitsiMeetViewReference jitsiMeetViewReference) {
         super(reactContext);
         mJitsiMeetViewReference = jitsiMeetViewReference;
+        registerForBroadcastEvents(reactContext);
     }
 
     @Override
@@ -41,17 +61,17 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                     if (userInfo != null) {
                         if (userInfo.hasKey("displayName")) {
                             _userInfo.setDisplayName(userInfo.getString("displayName"));
-                          }
-                          if (userInfo.hasKey("email")) {
+                        }
+                        if (userInfo.hasKey("email")) {
                             _userInfo.setEmail(userInfo.getString("email"));
-                          }
-                          if (userInfo.hasKey("avatar")) {
+                        }
+                        if (userInfo.hasKey("avatar")) {
                             String avatarURL = userInfo.getString("avatar");
                             try {
                                 _userInfo.setAvatar(new URL(avatarURL));
                             } catch (MalformedURLException e) {
                             }
-                          }
+                        }
                     }
                     RNJitsiMeetConferenceOptions options = new RNJitsiMeetConferenceOptions.Builder()
                             .setRoom(url)
@@ -74,17 +94,17 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                     if (userInfo != null) {
                         if (userInfo.hasKey("displayName")) {
                             _userInfo.setDisplayName(userInfo.getString("displayName"));
-                          }
-                          if (userInfo.hasKey("email")) {
+                        }
+                        if (userInfo.hasKey("email")) {
                             _userInfo.setEmail(userInfo.getString("email"));
-                          }
-                          if (userInfo.hasKey("avatar")) {
+                        }
+                        if (userInfo.hasKey("avatar")) {
                             String avatarURL = userInfo.getString("avatar");
                             try {
                                 _userInfo.setAvatar(new URL(avatarURL));
                             } catch (MalformedURLException e) {
                             }
-                          }
+                        }
                     }
                     RNJitsiMeetConferenceOptions options = new RNJitsiMeetConferenceOptions.Builder()
                             .setRoom(url)
@@ -108,4 +128,75 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
             }
         });
     }
+
+    private void registerForBroadcastEvents(Context context) {
+        IntentFilter intentFilter = new IntentFilter();
+
+        /* This registers for every possible event sent from JitsiMeetSDK
+           If only some of the events are needed, the for loop can be replaced
+           with individual statements:
+           ex:  intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.getAction());
+                intentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.getAction());
+                ... other events
+         */
+        for (BroadcastEvent.Type type : BroadcastEvent.Type.values()) {
+            intentFilter.addAction(type.getAction());
+        }
+
+        LocalBroadcastManager.getInstance(context).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void onBroadcastReceived(Intent intent) {
+        if (intent != null) {
+            BroadcastEvent event = new BroadcastEvent(intent);
+
+            switch (event.getType()) {
+                case CONFERENCE_JOINED:
+                    Timber.e("Conference Joined with url%s", event.getData().get("url"));
+                    break;
+                case PARTICIPANT_JOINED:
+                    Timber.e("Participant joined%s", event.getData().get("name"));
+                    break;
+                case ENDPOINT_TEXT_MESSAGE_RECEIVED:
+                    Timber.e("ENDPOINT_TEXT_MESSAGE_RECEIED" + event.getData().keySet());
+                    break;
+                case AUDIO_MUTED_CHANGED:
+                    Timber.e("AUDIO_MUTED_CHANGED" + event.getData().keySet());
+                    break;
+            }
+        }
+
+    }
+
+    @ReactMethod
+    public void muteUmuteAudio(boolean muted) {
+
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mJitsiMeetViewReference.getJitsiMeetView() != null) {
+                    Intent audioBroadcastIntent = BroadcastIntentHelper.buildSetAudioMutedIntent(muted);
+                    LocalBroadcastManager.getInstance(getReactApplicationContext()).sendBroadcast(audioBroadcastIntent);
+                    Timber.e("Audio Mute status changes to: " + muted);
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void sendMessage(String particepant, String message) {
+
+        UiThreadUtil.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mJitsiMeetViewReference.getJitsiMeetView() != null) {
+                    Intent messageBroadcastIntent = BroadcastIntentHelper.buildSendEndpointTextMessageIntent(particepant, message);
+                    LocalBroadcastManager.getInstance(getReactApplicationContext()).sendBroadcast(messageBroadcastIntent);
+                    Timber.e("Message sent: " + message);
+                }
+            }
+        });
+    }
+
+
 }
